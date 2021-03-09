3a1cf8033b49b0fde39b236a1167675430a6170e
Ajit <ajitsnz@gmail.com>
2021-03-09 21:50:02 +1300

    data driven test

---
+++ TestProjectWP/Ajit/Tests/BaseTest.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace TestProjectWP.Ajit.pages
+{
+    public class BaseTest
+    {
+        public IWebDriver browser;
+
+        [SetUp]
+        public void Setup()
+        {
+            browser = new ChromeDriver();
+            browser.Url = "http://qaauto.co.nz/test-blank-form/";
+            browser.FindElement(By.Id("wpforms-locked-16-field_form_locker_password")).SendKeys("Testing");
+            browser.FindElement(By.Id("wpforms-submit-locked-16")).Click();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            browser.Quit();
+
+        }
+
+    }
+}
